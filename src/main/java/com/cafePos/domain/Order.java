676c9f676b1194f2f578c1541d1ca49934626401
mkornam <mkornacki888@gmail.com>
2025-09-23 19:26:54 +0100

    did some stuff

--- src/main/java/com/cafePos/domain/Order.java
+++ src/main/java/com/cafePos/domain/Order.java
@@ -2,11 +2,12 @@ package com.cafePos.domain;
 
 import com.cafePos.common.Money;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
 public final class Order {
-    private final long id;
+    public final long id;
     private final List<LineItem> items = new ArrayList<>();
     public Order(long id) { this.id = id; }
     public void addItem(LineItem li) {
@@ -15,14 +16,8 @@ public final class Order {
         }
     }
     public Money subtotal() {
-        Money subtotal;
-      for(int i =0;i<items.size();i++){
-         LineItem lineItem =  items.get(i);
-
-         subtotal += lineItem.lineTotal();
-
-        }
-        return subtotal;
+        return items.stream().map(LineItem::lineTotal).reduce(Money.zero()
+                , Money::add);
     }
     public Money taxAtPercent(int percent) {
 
@@ -33,4 +28,13 @@ public final class Order {
         return subtotal ().add(taxAtPercent(percent));
 
     }
+
+    public long getID(){
+        return id;
+    }
+
+    public List<LineItem> getItems(){
+        return items;
+    }
+
 }
