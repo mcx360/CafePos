48d943b4e29abb04dec89da3acfc46e5e4e72b07
VivianVinod Jently <22079017@studentmail.ul.ie>
2025-11-10 17:55:27 +0000

    Week09 Lab part B,C finished

---
+++ src/main/java/com/cafePos/demo/Week9Demo_State.java
@@ -0,0 +1,15 @@
+package com.cafePos.demo;
+
+import com.cafePos.state.OrderFSM;
+public final class Week9Demo_State {
+    public static void main(String[] args) {
+        OrderFSM fsm = new OrderFSM();
+        System.out.println("Status = " + fsm.status());
+        fsm.prepare(); // invalid before pay
+        fsm.pay(); // NEW -> PREPARING
+        fsm.prepare(); // still preparing
+        fsm.markReady(); // PREPARING -> READY
+        fsm.deliver(); // READY -> DELIVERED
+        System.out.println("Status = " + fsm.status());
+    }
+}
---
+++ src/main/java/com/cafePos/state/CancelledState.java
@@ -0,0 +1,14 @@
+package com.cafePos.state;
+
+final class CancelledState implements State {
+    @Override public void pay(OrderFSM ctx) { System.out.println("[State] Cancelled"); }
+        @Override public void prepare(OrderFSM ctx) {
+            System.out.println("[State] Cancelled"); }
+        @Override public void markReady(OrderFSM ctx) {
+            System.out.println("[State] Cancelled"); }
+        @Override public void deliver(OrderFSM ctx) {
+            System.out.println("[State] Cancelled"); }
+        @Override public void cancel(OrderFSM ctx) {
+            System.out.println("[State] Already cancelled"); }
+        @Override public String name() { return "CANCELLED"; }
+    }
